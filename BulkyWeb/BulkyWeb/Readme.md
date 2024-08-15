263c88c67492962f6d780431b4ceb4e50b5cdc04
BharathCode1 <bharathcode1@gmail.com>
2024-08-15 20:17:58 +0530

    add read file

---
+++ BulkyWeb/BulkyWeb/Readme.md
@@ -0,0 +1,9 @@
+# .NET MVC
+
+## File structures
+
+* wwwroot -> static files like javascript, css, html, images
+
+## Entity Framework Tools
+
+### Migraion commands
--- BulkyWeb/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+088b4c0d7b2945908df491e9fe7f8e0dcce04f70")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
